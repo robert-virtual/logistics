78df5e06b59d16ba73eb00c9e043f24d17879924
robertocastillotng <roberto.castillo@tengo.hn>
2025-11-20 23:05:01 -0600

    basename for router

--- app/root.tsx
+++ app/root.tsx
@@ -26,7 +26,7 @@ export const links: Route.LinksFunction = () => [
   },
   {
     rel: "manifest",
-    href: "/manifest.webmanifest",
+    href: "/logistics/manifest.webmanifest",
   },
 ];
 
@@ -39,6 +39,7 @@ export function Layout({ children }: { children: React.ReactNode }) {
     <html lang="en">
       <head>
         <meta charSet="utf-8" />
+        <base href="/logistics/" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <Meta />
         <Links />
